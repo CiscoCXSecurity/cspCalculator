18b7f6c01a8d20a0a069532089951586b29d3614
timb <timb@da283472-e30e-4bd4-a68d-706198139375>
2013-11-28 23:49:12 +0000

    Updated src/html/examples/php/index.php to use new UI components and to allow for dynamic HTML body changes
    git-svn-id: https://projects.nth-dimension.org.uk/subversion/cspCalculator/trunk@84 da283472-e30e-4bd4-a68d-706198139375

--- src/html/examples/php/index.php
+++ src/html/examples/php/index.php
@@ -1,6 +1,6 @@
 <?php
 	/*
-	$Header: /var/lib/cvsd/var/lib/cvsd/cspCalculator/src/html/examples/php/index.php,v 1.3 2013-04-22 22:11:06 timb Exp $
+	$Header: /var/lib/cvsd/var/lib/cvsd/cspCalculator/src/html/examples/php/index.php,v 1.4 2013-11-28 23:49:12 timb Exp $
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -37,18 +37,52 @@
 	//if (!preg_match("/.*" . $protocol . "\:\/\/" . $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"], urldecode($_COOKIE["script-src"]))) {
 	//	$_COOKIE["script-src"] .= " " . $protocol . "://" . $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"];
 	//}
-	if (!preg_match("/.*'self.'*/", urldecode($_COOKIE["script-src"]))) {
+	if (!preg_match("/.*'self'.*/", urldecode($_COOKIE["script-src"]))) {
 		$_COOKIE["script-src"] .= " 'self'";
 	}
-	if ($_COOKIE["sandbox"] == "") {
+	if (!preg_match("/.*'self'.*/", urldecode($_COOKIE["style-src"]))) {
+		$_COOKIE["style-src"] .= " 'self'";
+	}
+	if (!preg_match("/.*allow-forms.*/", urldecode($_COOKIE["sandbox"]))) {
 		$_COOKIE["sandbox"] = " allow-forms";
+	}
+	if (!preg_match("/.*allow-same-origin.*/", urldecode($_COOKIE["sandbox"]))) {
 		$_COOKIE["sandbox"] .= " allow-same-origin";
+	}
+	if (!preg_match("/.*allow-scripts.*/", urldecode($_COOKIE["sandbox"]))) {
 		$_COOKIE["sandbox"] .= " allow-scripts";
+	}
+	if (!preg_match("/.*allow-top-navigation.*/", urldecode($_COOKIE["sandbox"]))) {
 		$_COOKIE["sandbox"] .= " allow-top-navigation";
+	}
+	if ((!preg_match("/.*cspCalculator\.js.*/", urldecode($_COOKIE["contents"]))) || (!preg_match("/.*htmlEditor\.js.*/", urldecode($_COOKIE["contents"])))) {
+		$_COOKIE["contents"] = rawurlencode("<html>
+	<head>
+		<title>cspCalculator</title>
+		<link rel=\"stylesheet\" href=\"styles/about.css\" type=\"text/css\"/>
+		<link rel=\"stylesheet\" href=\"styles/copyright.css\" type=\"text/css\"/>
+		<link rel=\"stylesheet\" href=\"styles/cspCalculator.css\" type=\"text/css\"/>
+		<link rel=\"stylesheet\" href=\"styles/htmlEditor.css\" type=\"text/css\"/>
+	</head>
+	<body>
+		<script src=\"js/about.js\"></script>
+		<script src=\"js/copyright.js\"></script>
+		<script src=\"js/cspCalculator.js\"></script>
+		<script src=\"js/htmlEditor.js\"></script>
+		<img src=\"http://www.machine.org.uk/images/thug.jpeg\">
+		<iframe src=\"http://comodosslstore.com/checksslcertificate.aspx\"></iframe>
+		<img src=\"/icons/alert.red.png\">
+		<object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0\" id=\"http://www.welcometointernet.org/Jesus%20Cat.swf\">
+			<param name=\"movie\" value=\"http://www.welcometointernet.org/Jesus%20Cat.swf\">
+			<embed src=\"http://www.welcometointernet.org/Jesus%20Cat.swf\" name=\"http://www.welcometointernet.org/Jesus%20Cat.swf\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\"></embed>
+		</object>
+	</body>
+</html>");
 	}
 	foreach ($directiveslist as $directivename) {
 		header("Set-Cookie: " . $directivename . "=" . $_COOKIE[$directivename], false);
 	}
+	header("Set-Cookie: contents=" . $_COOKIE["contents"], false);
 	foreach ($headerslist as $headername) {
 		$headervalue = "";
 		foreach ($directiveslist as $directivename) {
@@ -56,17 +90,5 @@
 		}
 		header($headername . ": " . $headervalue);
 	}
+	echo urldecode($_COOKIE["contents"]);
 ?>
-<html>
-<body>
-<script src="../../js/cspCalculator.js"></script>
-<img src="http://www.machine.org.uk/images/thug.jpeg">
-<iframe src="http://sarpedon.local/"></iframe>
-<img src="/icons/alert.red.png">
-<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" id="http://www.welcometointernet.org/Jesus%20Cat.swf">
-	<param name="movie" value="http://www.welcometointernet.org/Jesus%20Cat.swf">
-	<embed src="http://www.welcometointernet.org/Jesus%20Cat.swf" name="http://www.welcometointernet.org/Jesus%20Cat.swf" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer">
-	</embed>
-</object>
-</body>
-</html>
